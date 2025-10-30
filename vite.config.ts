c6d8efc9174d291c69271ffb50d3f040a352823f
Dan Epstein <25646665+Depsteino@users.noreply.github.com>
2025-10-30 14:25:08 +0200

    Improve cross-browser compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => ({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES_BASE ?? "/my-bio-vite/",
+  base: process.env.GITHUB_PAGES_BASE ?? "/",
   build: {
+    target: ["es2018", "edge90", "firefox78", "safari13"],
+    cssTarget: ["chrome61", "edge90", "firefox78", "safari13"],
     outDir: "dist",
     sourcemap: true
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2018"
+    }
   }
 }));
